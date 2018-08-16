dfa5706ec5f3206bb8a0dc69c033001aa88e800a
Yury Sukhoverkhov <y@vitasw.com>
2018-08-16 20:06:25 +0200

    Fix bug with loading resources from another thread

--- common/src/main/scala/com/example/airports/persistence/sourcealg/SourceResourceInterpreter.scala
+++ common/src/main/scala/com/example/airports/persistence/sourcealg/SourceResourceInterpreter.scala
@@ -9,11 +9,13 @@ import com.example.airports.persistence.SourceAlg
 // Resource source for CSV data
 class SourceResourceInterpreter(resource: String) extends SourceAlg[IO] {
 
+  private val classLoader = Thread.currentThread().getContextClassLoader
+
   override def source: EitherT[IO, ErrorReason, String] = {
     EitherT {
       IO {
         Either.catchNonFatal {
-          scala.io.Source.fromResource(resource).mkString
+          scala.io.Source.fromResource(resource, classLoader).mkString
         }.leftMap(ErrorReason.fromThrowable(s"Error loading resource [$resource]"))
       }
     }
